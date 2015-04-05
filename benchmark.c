8cc2ecd61698a8244d5cd2745f750867593b30fb
linecker <linecker@gmail.com>
2015-04-05 12:10:11 +0200

    Fix size comparision.

--- benchmark.c
+++ benchmark.c
@@ -1,37 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define colon(i, l, s) character(':', i, l, s)
+#define point(i, l, s) character('.', i, l, s)
+
 inline bool digit(int& i, char* line, const size_t size) 
 {
 	char c = line[i];
 	if (c <= '0' || c >= '9')
 		return false;
-	if (i >= size)
+	if (i > size)
 		return false;
 	i++;
 	return true;
 }
 
-inline bool colon(int& i, char* line, const size_t size) 
+inline bool character(char which, int& i, char* line, const size_t size)
 {
 	char c = line[i];
-	if (c != ':')
+	if (c != which)
 		return false;
-	if (i >= size)
-		return false;
-	i++;
-	return true;
-}
-
-inline bool point(int& i, char* line, const size_t size) 
-{
-	char c = line[i];
-	if (c != '.')
-		return false;
-	if (i >= size)
+	if (i > size)
 		return false;
 	i++;
-	return true;
+	return true;	
 }
 
 int main()
@@ -39,7 +31,7 @@ int main()
 	char* line = NULL;
 	size_t size;
 	while (getline(&line, &size, stdin) != -1) {
-		printf("%s", line);
+		bool hit = false;
 		for (int i = 0; i < size; i++) {
 			// look for hh:mm:ss.mmm	
 			if (digit(i, line, size) &&
@@ -54,11 +46,17 @@ int main()
 				digit(i, line, size) &&
 				digit(i, line, size) &&
 				digit(i, line, size)) {
-				printf("timestamp found!");	
+				hit = true;
+				break;	
 			} else {
 				continue;
 			}
 		}
+		if ( hit == true )
+			printf("*");
+		else 
+			printf(" ");
+		printf("%s", line);
 	}
 	return 0;
 }
